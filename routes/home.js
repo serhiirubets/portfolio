3a3c15c755d584bd17494e0fd48de85923d50afd
serhii.rubets <serhii.rubets@playtika.com>
2020-06-22 22:27:59 +0300

    Add header

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,26 @@
 const {Router} = require('express')
 const router = Router()
 
+const links = [
+  {
+    icon: 'facebook',
+    url: 'https://www.facebook.com/serhiirubets'
+  },
+  {
+    icon: 'github',
+    url: 'https://github.com/serhiirubets'
+  },
+  {
+    icon: 'in',
+    url: 'https://www.instagram.com/serhii_rubets'
+  }
+]
+
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Главная страница',
-    isHome: true
+    title: 'Обо мне',
+    isHome: true,
+    links,
   })
 })
 
